6e50292c02ee7f6a2f3bc8efb46ad5dbf9911857
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-06-16 14:02:08 +0300

    add structure

---
+++ controllers/contacts/getContactById.js
@@ -0,0 +1,18 @@
+const Contact = require("../../models/contact");
+
+const { HttpError } = require("../../helpers");
+
+const { ctrlWrapper } = require("../../decorators");
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getContactById: ctrlWrapper(getContactById),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const contactsController = require("../../controllers/contacts-controller");
+const contactsController = require("../../controllers/contacts");
 
 const schemas = require("../../schemas/contacts");
 const { validateBody } = require("../../decorators/validateBody");
